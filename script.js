868c4574beaa678245dfdbcd4ff886721742b29a
Vlad_ivn <vladislavivaniuk@gmail.com>
2023-10-21 21:09:48 +0300

    first

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+      const burgerMenu = document.querySelector('.burger-menu');
+      const menu = document.querySelector('.menu');
+
+      burgerMenu.addEventListener('click', function () {
+        this.classList.toggle('open');
+        menu.classList.toggle('open');
+      });
+    });
